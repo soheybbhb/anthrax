31bb708f0e370a6c9cd2aa7bc2d34424eca84bdf
soheybbhb <167059587+soheybbhb@users.noreply.github.com>
2024-04-15 08:16:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.image img');
+let container = document.querySelector('.container');
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.backgroundColor = color;
+}  
+
